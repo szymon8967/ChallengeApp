92dcb349c249c12c5a3d08e74384047389b06751
Szymon8967 <szymon.pisula@op.pl>
2023-09-07 17:44:15 +0200

    Zadanie domowe - dzień 4.

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var imie = "Ewa";
+char plec = 'k';
+var wiek = 33;
+
+if (plec == 'k' && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (wiek < 18 && plec == 'm')
+{ 
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
