d58d5f708b5780c3af2c34a0073c634d4fe9a114
Szymon8967 <szymon.pisula@op.pl>
2023-09-09 18:57:20 +0200

    Zadanie domowe - dzień 5.

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,16 +1,52 @@
-﻿var imie = "Ewa";
-char plec = 'k';
-var wiek = 33;
-
-if (plec == 'k' && wiek < 30)
+﻿int[] counters = new int[10];
+int number = 4566;
+Console.WriteLine(number);
+string numberAsString = number.ToString();
+char[] signs = numberAsString.ToCharArray();
+foreach (char sign in signs)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    if (sign == '0')
+    {
+        counters[0] ++;
+    }
+    else if (sign == '1') 
+    {
+        counters[1]++;
+    }
+    else if (sign == '2')
+    {
+        counters[2]++;
+    }
+    else if (sign == '3')
+    {
+        counters[3]++;
+    }
+    else if (sign == '4')
+    {
+        counters[4]++;
+    }
+    else if (sign == '5')
+    {
+        counters[5]++;
+    }
+    else if (sign == '6')
+    {
+        counters[6]++;
+    }
+    else if (sign == '7')
+    {
+        counters[7]++;
+    }
+    else if (sign == '8')
+    {
+        counters[8]++;
+    }
+    else if (sign == '9')
+    {
+        counters[9]++;
+    }
 }
-else if (imie == "Ewa" && wiek == 33)
+for(var i=0; i<counters.Length; i++)
 {
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (wiek < 18 && plec == 'm')
-{ 
-    Console.WriteLine("Niepełnoletni Mężczyzna");
+    Console.WriteLine(i+"->"+counters[i]);
 }
